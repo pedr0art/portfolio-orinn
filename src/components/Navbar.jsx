02f577ae68c37e579c8e95dea94b16e11dc457ee
pedr0art <pedroart3001@gmail.com>
2025-07-13 22:54:31 -0300

    logo adição

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,36 @@ const navItems = [
   { name: "Fale conosco", href: "#contact" },
 ];
 
-
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(
+    document.documentElement.classList.contains("dark")
+  );
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.screenY > 10);
+      setIsScrolled(window.scrollY > 10);
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      const isDark = document.documentElement.classList.contains("dark");
+      setIsDarkMode(isDark);
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <nav
       className={cn(
@@ -31,14 +48,16 @@ export const Navbar = () => {
       )}
     >
       <div className="container flex items-center justify-between">
-        <a
-          className="text-xl font-bold text-primary flex items-center"
-          href="#hero"
-        >
-          <span className="relative z-10">
-            <span className="text-glow text-foreground"> Portfólio </span>{" "}
-            Orinn Studio
-          </span>
+        <a href="#hero" className="flex items-center">
+          <img
+            src={
+              isDarkMode
+                ? "/orinn/logo_orinn_dark.svg"
+                : "/orinn/logo_orinn_light.svg"
+            }
+            alt="Orinn Studio Logo"
+            className="h-20 w-auto transition-all duration-300"
+          />
         </a>
 
         {/* desktop nav */}
@@ -55,18 +74,17 @@ export const Navbar = () => {
         </div>
 
         {/* mobile nav */}
-
         <button
           onClick={() => setIsMenuOpen((prev) => !prev)}
           className="md:hidden p-2 text-foreground z-50"
           aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
         >
-          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
 
         <div
           className={cn(
-            "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center",
+            "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
             "transition-all duration-300 md:hidden",
             isMenuOpen
               ? "opacity-100 pointer-events-auto"
